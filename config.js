437e8ac4fc884a315ef9b9b52565ce921d6fe40b
zhaoyoushuai <1164375084@qq.com>
2019-03-29 22:24:42 +0800

    init

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 var fs = require("fs");
 
 var globalConfig = {};
-
+let os = require("os")
 
 var conf = fs.readFileSync("./server.conf");
-var confArr = conf.toString().split("\r\n");
+var confArr = conf.toString().split(os.EOL);
 console.log(confArr)
 for(var i = 0 ; i < confArr.length;i ++){
     if(confArr[i]){
