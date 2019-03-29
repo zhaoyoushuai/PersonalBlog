a057f91e801ab59d3c8cfc994edae60b9a7c4f7e
zhaoyoushuai <1164375084@qq.com>
2019-03-29 22:23:30 +0800

    init

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ var globalConfig = {};
 
 var conf = fs.readFileSync("./server.conf");
 var confArr = conf.toString().split("\r\n");
+console.log(confArr)
 for(var i = 0 ; i < confArr.length;i ++){
     if(confArr[i]){
         globalConfig[confArr[i].split("=")[0]] = confArr[i].split("=")[1]
